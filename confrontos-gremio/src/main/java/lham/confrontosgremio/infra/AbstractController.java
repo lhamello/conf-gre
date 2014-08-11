7325a93591cb016f877b24bf35d4cf2492ed9573
lhamello <lhamello@gmail.com>
2014-08-11 00:03:04 -0300

    Messages update

--- confrontos-gremio/src/main/java/lham/confrontosgremio/infra/AbstractController.java
+++ confrontos-gremio/src/main/java/lham/confrontosgremio/infra/AbstractController.java
@@ -2,8 +2,27 @@ package lham.confrontosgremio.infra;
 
 import java.io.Serializable;
 
-public abstract class AbstractController implements Serializable {
+import lham.confrontosgremio.util.FacesUtil;
+import lham.confrontosgremio.util.MessageBundleUtil;
+
+public abstract class AbstractController<E extends AbstractModel<K>, K> implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
+    private AbstractService<E, K> service;
+    private E entity;
+    private K id;
+
+    public void saveOrUpdate() {
+        if (entity.getId() == null) {
+            entity = service.insert(entity);
+            FacesUtil.addInfoMessage(MessageBundleUtil.getMessage("registro.inclui-sucesso"));
+        } else {
+            entity = service.altera(entity);
+            FacesUtil.addInfoMessage(MessageBundleUtil.getMessage("registro.altera-sucesso"));
+        }
+        
+        entity = service.findById(entity.getId());
+    }
+
 }
